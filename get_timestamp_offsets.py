714019b20d8471e261dc954efb4632728c25e6ed
Adam Lis <adambricelis@gmail.com>
2025-03-08 21:11:34 -0500

    Simplify output of get_timestamp_offsets.py
    Only output the timestamp offsets of the earliest and latest timestamps.

--- get_timestamp_offsets.py
+++ get_timestamp_offsets.py
@@ -9,16 +9,22 @@ DESIRED_TIMESTAMP = '2022:05:28 14:00:00'
 
 image_paths = [os.path.join(IMAGES_DIRECTORY, entry) for entry in os.listdir(IMAGES_DIRECTORY) if os.path.isfile(os.path.join(IMAGES_DIRECTORY, entry))]
 
+image_timestamps = {}
+
 for path in image_paths:
-    print(path)
     with open(path, 'rb') as file:
         image = Image(file)
+        timestamp = datetime.strptime(image.get(TIMESTAMP), TIMESTAMP_FORMAT)
+        image_timestamps[path] = timestamp
 
-        original_timestamp = datetime.strptime(image.get(TIMESTAMP), TIMESTAMP_FORMAT)
-        print(f'Original: {original_timestamp}')
+desired_timestamp = datetime.strptime(DESIRED_TIMESTAMP, TIMESTAMP_FORMAT)
+min_timestamp = min(image_timestamps.values())
+min_timestamp_delta = desired_timestamp - min_timestamp
+max_timestamp = max(image_timestamps.values())
+max_timestamp_delta = desired_timestamp - max_timestamp
 
-        modified_timestamp = datetime.strptime(DESIRED_TIMESTAMP, TIMESTAMP_FORMAT)
-        print(f'Modified: {modified_timestamp}')
-
-        delta = modified_timestamp - original_timestamp
-        print(delta)
+print(f'Desired timestamp: {desired_timestamp}')
+print(f'Earliest timestamp: {min_timestamp}')
+print(f'Latest timestamp: {max_timestamp}')
+print(f'Difference between earliest and desired timestamps: {min_timestamp_delta}')
+print(f'Difference between latest and desired timestamps: {max_timestamp_delta}')
